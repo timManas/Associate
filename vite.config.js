b793d7281e023a902c9c1946e5d290ee9dbc6da5
Tim M <timothy.romero.manas@gmail.com>
2024-02-19 18:03:10 -0500

    Adding proxy to vite proxy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,19 @@ import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
+// export default defineConfig({
+//   base: './',
 
-  plugins: [react(), eslint()],
+//   plugins: [react(), eslint()],
+// })
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // proxy requests prefixed '/api' and '/uploads'
+    proxy: {
+      '/api': 'http://localhost:5000',
+      '/uploads': 'http://localhost:5000',
+    },
+  },
 })
