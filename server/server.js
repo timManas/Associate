a05b0ff4f40ac0916ef338e40e9d8af24018920f
Tim M <timothy.romero.manas@gmail.com>
2024-02-19 00:44:20 -0500

    Modify server file to allow path

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import dotenv from 'dotenv'
 dotenv.config({ path: '../.env' })
 
@@ -24,10 +25,10 @@ app.use('/subscriber', subscriberRoutes)
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve()
   app.use('/uploads', express.static('/var/data/uploads'))
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '/client/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   )
 } else {
   const __dirname = path.resolve()
