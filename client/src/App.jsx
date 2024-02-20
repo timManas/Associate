ed2f8baec2104be96d2c214839deb01c515c1f17
Tim M <timothy.romero.manas@gmail.com>
2024-02-19 23:33:13 -0500

    Adding custom Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import { Outlet } from 'react-router-dom'
+import HeaderCustom from './components/HeaderCustom.jsx'
 
 function App() {
   return (
     <>
-      <Header />
+      <HeaderCustom />
       <Outlet />
       <Footer />
     </>
